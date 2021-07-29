1a47ca7c5534c2d2de6111242304addddc2c0060
Vadim Bush <bushmvadim@gmail.com>
2021-07-29 13:04:09 +0300

    Add all five fragments(only declaration) for navigation

---
+++ app/src/main/java/com/example/bushv/example/presentation/fragments/CompletedThemesFragment.kt
@@ -0,0 +1,6 @@
+package com.example.bushv.example.presentation.fragments
+
+import androidx.fragment.app.Fragment
+
+class CompletedThemesFragment: Fragment() {
+}
---
+++ app/src/main/java/com/example/bushv/example/presentation/fragments/ThemeChooserFragment.kt
@@ -0,0 +1,7 @@
+package com.example.bushv.example.presentation.fragments
+
+import androidx.fragment.app.Fragment
+
+class ThemeChooserFragment: Fragment() {
+
+}
