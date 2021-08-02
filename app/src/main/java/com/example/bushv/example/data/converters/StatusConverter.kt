7b818a46c07553bfea8254347b327ed68185ea22
Vadim Bush <bushmvadim@gmail.com>
2021-08-02 08:17:31 +0300

    fix Status converter class

--- app/src/main/java/com/example/bushv/example/data/converters/StatusConverter.kt
+++ app/src/main/java/com/example/bushv/example/data/converters/StatusConverter.kt
@@ -3,19 +3,17 @@ package com.example.bushv.example.data.converters
 import androidx.room.TypeConverter
 import com.example.bushv.example.domain.entity.Status
 
+
 class StatusConverter {
 
-    class StatusConverter {
+    @TypeConverter
+    fun toStatus(position: Int): Status {
+        return Status.values()[position]
+    }
 
-        @TypeConverter
-        fun toStatus(position: Int): Status {
-            return Status.values()[position]
-        }
-
-        @TypeConverter
-        fun toInt(status: Status): Int {
-            return status.ordinal
-        }
+    @TypeConverter
+    fun toInt(status: Status): Int {
+        return status.ordinal
     }
 
 }
