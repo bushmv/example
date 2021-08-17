28c95a86eedd698027ba4e39ac32ad18e270aab1
Vadim Bush <bushmvadim@gmail.com>
2021-08-17 18:07:43 +0300

    Add unit tests

---
+++ app/src/test/java/com/example/bushv/example/data/TestAppDatabase.kt
@@ -0,0 +1,34 @@
+package com.example.bushv.example.data
+
+import com.example.bushv.example.domain.entity.EnglishLevel
+import com.example.bushv.example.domain.entity.Example
+import com.example.bushv.example.domain.entity.Status
+import com.example.bushv.example.domain.entity.Theme
+
+class TestAppDatabase: AppDatabase{
+
+    val englishLevelsLoadedThemes = arrayListOf<EnglishLevel>()
+
+    companion object {
+        val mockTheme = Theme(-1, "", "", EnglishLevel.BEGINNER, Status.THEORY, 0, 0, 0f)
+    }
+
+    override suspend fun loadTheme(englishLevel: EnglishLevel, offset: Int): Theme {
+        englishLevelsLoadedThemes.add(englishLevel)
+        return mockTheme
+    }
+
+    override suspend fun themesCount(englishLevel: EnglishLevel): Int {
+        return 1
+    }
+
+    override suspend fun updateTheme(theme: Theme) {}
+
+    override suspend fun completedThemesFor(englishLevel: EnglishLevel): List<Theme> { return emptyList() }
+
+    override suspend fun loadExamplesWithThemeId(themeId: Int): List<Example> { return emptyList() }
+
+    override suspend fun updateExample(example: Example) {}
+
+    override suspend fun favoriteExamples(): List<Example> { return emptyList() }
+}
